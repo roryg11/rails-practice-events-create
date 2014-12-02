2f93ca39b85cb52472525f7274555c0e6c07cb3b
roryg11 <Rory.C.Grant@gmail.com>
2014-12-02 11:59:54 -0800

    adding edit and update capabilities

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,6 +16,19 @@ class EventsController < ApplicationController
     @events=Event.all
   end
 
+  def edit
+    @event=Event.find(params[:id])
+  end
+
+  def update
+    @event=Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to events_path
+    else
+      render :edit
+    end
+  end
+
   private
   def event_params
     params.require(:event).permit(
